46bae6e91edce7554f3e70edd5d37a2190e0cfca
Eric <VGCbolt@gmail.com>
2017-05-23 16:32:49 -0400

    Make things so, so pretty

---
+++ src/ThingList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './ThingList.css'
+import Thing from './Thing'
+
+const ThingList = (props) => {
+    return (
+        <ul className="ThingList">
+            {
+                Object
+                    .keys(props.things)
+                    .map(thingID => <Thing thing={props.things[thingID]} key={thingID}/>)
+            }
+        </ul>
+    )
+}
+
+export default ThingList
