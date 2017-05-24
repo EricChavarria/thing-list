5744260db575ecc71de6e0ea0c5341e612b663f5
Eric <VGCbolt@gmail.com>
2017-05-24 15:32:06 -0400

    Autofocus new things

--- src/Thing.js
+++ src/Thing.js
@@ -5,11 +5,16 @@ import './Thing.css'
 import Actions from './Actions'
 
 class Thing extends Component {
+    componentDidMount() {
+        this.nameInput.htmlEl.focus()
+    }
+
     updateName = (ev) => {
         const { thing, saveThing } = this.props
         thing.name = ev.target.value
         saveThing(thing)
     }
+
     render() {
         const { thing, removeThing } = this.props
         return (
@@ -20,6 +25,7 @@ class Thing extends Component {
                                 className="name"
                                 html={thing.name}
                                 onChange={this.updateName}
+                                ref={input => this.nameInput = input}
                             />
                         <Actions
                             thing ={thing} 
