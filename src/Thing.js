2eabbc0d57f55b558af665e97ccb465c5068c93b
Eric <VGCbolt@gmail.com>
2017-05-24 16:17:22 -0400

    fix auto focus

--- src/Thing.js
+++ src/Thing.js
@@ -6,7 +6,9 @@ import Actions from './Actions'
 
 class Thing extends Component {
     componentDidMount() {
-        this.nameInput.htmlEl.focus()
+        if(!this.nameInput.htmlEl.textContext) {
+            this.nameInput.htmlEl.focus()
+        }
     }
 
     updateName = (ev) => {
